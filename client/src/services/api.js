b9b1cec1b9b44e756b0181dc037357bd69761f13
bhanusreevankayala <bhanusreevankayala@gmail.com>
2026-05-27 09:58:31 +0530

    frontend deployment setup

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "hhttps://campus-serve-backend.onrender.com",
 });
 
 export default API;
